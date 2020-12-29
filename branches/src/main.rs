df3b53670dac21f650b8bf620925e5b0a91847ac
Derrick Carr <whyderrick@gmail.com>
2020-12-28 23:34:50 -0800

    Initial commit with old run material

---
+++ branches/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = 9;
+
+    if number {
+        println!("Condition was true!");
+    } else {
+        println!("Condition was false!");
+    }
+}
---
+++ loops/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let a = [10, 20, 30 , 40, 50];
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
